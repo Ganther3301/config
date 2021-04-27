2aeb1e8f3541096545ec65e1d01c62d9b52f8b88
Ganther3301 <hithesh2k2@gmail.com>
2021-04-27 18:42:12 +0530

    commit

--- import.py
+++ import.py
@@ -5,3 +5,11 @@ files = ['i3', 'i3blocks', 'nvim', 'polybar', 'qtile']
 
 for i in files:
     subprocess.run("cp -r ~/.config/"+i+" ~/config", shell=True) 
+
+gitCommands = [
+    'git add .',
+    'git commit -m "commit"',
+    'git push'
+]
+for i in gitCommands:
+    subprocess.run(i, shell=True)
